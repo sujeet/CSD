cb89cf72ed24fceb7739ff56e5c9afe71f185a95
S Pradeep Kumar <gohanpra@gmail.com>
2012-09-14 18:34:26 +0530

    Added code for trying out different permutations of caches.

--- Simulator.py
+++ Simulator.py
@@ -19,10 +19,12 @@ Usage:
 
 $ ./cache_simulator.py [options]
 
--f --trace-file : name of the trace file (mem.trace default)
--b --block-size : block size for the cache in bytes (1K default)
--c --cache-size : size of the cache in bytes (1M default)
--w --ways       : number of ways in the cache (16 default)
+-f --trace-file: name of the trace file (mem.trace default)
+-b --block-size: block size for the cache in bytes (1K default)
+-c --cache-size: size of the cache in bytes (1M default)
+-w --ways      : number of ways in the cache (16 default)
+
+Note: All byte numbers should be non-negative powers of 2.
 """
     try:
         opts, args = getopt.getopt (argv,
@@ -42,36 +44,35 @@ $ ./cache_simulator.py [options]
         elif opt in ("-f", "--trace-file"):
             file_name = arg
         elif opt in ("-b", "--block-size"):
-            try :
+            try:
                 block_size = int(arg)
-            except :
-                if arg [-1] == 'M' :
+            except:
+                if arg [-1] == 'M':
                     block_size = int (arg [:-1]) * 1048576
-                if arg [-1] == 'K' :
+                if arg [-1] == 'K':
                     block_size = int (arg [:-1]) * 1024
         elif opt in ("-c", "--cache-size"):
-            try :
+            try:
                 cache_size = int(arg)
-            except :
-                if arg [-1] == 'M' :
+            except:
+                if arg [-1] == 'M':
                     cache_size = int (arg [:-1]) * 1048576
-                if arg [-1] == 'K' :
+                if arg [-1] == 'K':
                     cache_size = int (arg [:-1]) * 1024
         elif opt in ("-w", "--ways"):
             num_ways = int(arg)
     
     num_sets =  (cache_size/block_size)/num_ways
 
-def simulate (cache, memtrace) :
-    for mode, addr in memtrace :
-        if mode == 'R' :
+def simulate (cache, memtrace):
+    for mode, addr in memtrace:
+        if mode == 'R':
             cache.read (addr)
-        elif mode == 'W' :
+        elif mode == 'W':
             cache.write (addr)
-    cache.printStats ()
-
-
-def genMemtrace (filename) :
+    # cache.printStats ()
+    
+def genMemtrace (filename):
     """ Returns a list of pairs, [r/w, addr] """
     tracefile = open (filename)
     trace = [tuple(line.strip().split()) for
@@ -81,18 +82,34 @@ def genMemtrace (filename) :
     return trace
     
 
-if __name__ == "__main__" :
+if __name__ == "__main__":
     readOptions (sys.argv [1:])
     memtrace = genMemtrace (file_name)
     sample_addr = memtrace [0][1]
-    for cache_type in [Cache,
-                       FIFOCache,
-                       LRUCache,
-                       LFUCache] :
-        cache = cache_type (num_sets,
-                            num_ways,
-                            block_size,
-                            sample_addr,
-                            write_no_allocate = True)
-        simulate (cache, memtrace)
-        print '\n\n'
+
+    cache_list = [Cache, FIFOCache, LRUCache, LFUCache]
+    true_false_list = [True, False]
+    matrix_size_list = range (10, 60, 10) # + [100]
+    block_size_list = [16, 64, 256]
+    ways_list = [1, 4, 16]
+
+    for cache_type in cache_list:
+        for write_no_allocate in true_false_list:
+            for matrix_size in matrix_size_list:
+                for block_size in block_size_list:
+                    for num_ways in ways_list:
+                        # cache = cache_type (num_sets,
+                        #                     num_ways,
+                        #                     block_size,
+                        #                     sample_addr,
+                        #                     write_no_allocate = True)
+                        
+                        print 'Yo, boyz!'
+                        # cache = cache_type ((cache_size / block_size) / num_ways,
+                        #                     num_ways,
+                        #                     block_size,
+                        #                     # TODO: Check if this is necessary
+                        #                     sample_addr,
+                        #                     write_no_allocate = write_no_allocate)
+                        simulate (cache, memtrace)
+                        # print '\n\n'
