e3237f5d8ce0b8ce3fdc2514de22b797a09a3b12
S Pradeep Kumar <gohanpra@gmail.com>
2012-09-16 23:49:25 +0530

    Wrote code to generate different graphs from given memtraces.
    + Added write_result_to_file to save output of simulation to a file.
    + Added plot_cache_graphs method to plot from stats in results files.
    + Moved simulation using all combinations into ye_old_simulation_attempt method.
    + Added getHitRate and getParameters method to Cache.
    + Added method to get human-readable size from byte size.

--- Cache.py
+++ Cache.py
@@ -4,17 +4,33 @@ import math
 import random
 import sys
 
-def pad (string, padded_length, padding_char = " ") :
+def pad (string, padded_length, padding_char = " "):
     return string + padding_char * (padded_length - len (string))
 
-class Set (object) :
+def getHumanReadableSize(size_in_bytes):
+    """Return human-readable version of size_in_bytes.
+
+    Suffix:
+    X > 1024 * 1024 => M
+    X > 1024 and X < 1024 * 1024 => K
+    X < 1024 => No suffix
+    """
+    human_readable_size = size_in_bytes
+    if human_readable_size >= 1048576:
+        human_readable_size = str (human_readable_size / 1048576) + 'M'
+    elif human_readable_size >= 1024:
+        human_readable_size = str (human_readable_size / 1024) + "K"
+    return str (human_readable_size)
+
+
+class Set (object):
     """Models the set as in the set-associative cache."""
     dirty, clean = 1, 0
 
     def __init__ (self,
                   size,
                   tag_bit_length,
-                  write_no_allocate) :
+                  write_no_allocate):
         self.dict = {}
         self.capacity = size
         self.seen_tags = set ()
@@ -24,13 +40,13 @@ class Set (object) :
         self.tag_bit_length = tag_bit_length
         self.write_no_allocate = write_no_allocate
 
-    def __str__ (self) :
+    def __str__ (self):
         """A suitably space separated list of tags in the set.
         Tags in hex, a * shows a dirty tag."""
         str_repr = ""
-        for key in self.dict :
+        for key in self.dict:
             modified = ' '
-            if self.dict [key] == self.dirty : modified = '*'
+            if self.dict [key] == self.dirty: modified = '*'
             str_repr += (
                 ' '
                 + pad (hex (key) [2:]
@@ -39,87 +55,87 @@ class Set (object) :
                 + ' ')
         return str_repr
 
-    def __writeBack (self, tag) :
+    def __writeBack (self, tag):
         # As we are just counting the number of misses and hits,
         # and not the performance in terms of time and latency etc,
         # What happens here is irrlevant.
         pass
 
-    def pickBlockToEvict (self) :
+    def pickBlockToEvict (self):
         return random.choice (self.dict.keys ())
 
-    def evictAndWrite (self, tag_to_be_written, dirty_status) :
+    def evictAndWrite (self, tag_to_be_written, dirty_status):
         """ Picks a tag to be evicted, writes its contents back
         to memory if dirty and then writes the tag_to_be_written
         to the set.
-        dirty_status : whether the block we just wrote to the cache
+        dirty_status: whether the block we just wrote to the cache
                        should be marked dirty or clean. """
         tag_to_replace = self.pickBlockToEvict ()
-        if self.dict [tag_to_replace] == self.dirty :
+        if self.dict [tag_to_replace] == self.dirty:
             self.__writeBack (tag_to_be_written)
         self.dict.pop (tag_to_replace)
         self.dict [tag_to_be_written] = dirty_status
 
-    def writeOrRead (self, tag, dirty_status) :
+    def writeOrRead (self, tag, dirty_status):
         self.access_count += 1
-        # Hit : Tag is present.
-        # Miss : Tag is not present
-        # -> tag not in seen tags : cold miss
-        # -> else if set is full : conflict miss
+        # Hit: Tag is present.
+        # Miss: Tag is not present
+        # -> tag not in seen tags: cold miss
+        # -> else if set is full: conflict miss
 
         # Hit
-        if tag in self.dict :
+        if tag in self.dict:
             pass
 
         # Also, don't do anything if it is a write miss and we are using
         # write no allocate policy. Just increment the counts.
-        elif self.write_no_allocate and dirty_status == self.dirty :
+        elif self.write_no_allocate and dirty_status == self.dirty:
             # Cold miss
-            if tag not in self.seen_tags :
+            if tag not in self.seen_tags:
                 self.cold_miss_count += 1
                 self.seen_tags.add (tag)
             # Conflict miss
-            else :
+            else:
                 self.conflict_miss_count += 1
 
         # Cold miss
-        elif tag not in self.seen_tags :
+        elif tag not in self.seen_tags:
             self.cold_miss_count += 1
-            if len (self.dict) == self.capacity :
+            if len (self.dict) == self.capacity:
                 self.evictAndWrite (tag, dirty_status)
-            else :
+            else:
                 self.dict [tag] = dirty_status
             self.seen_tags.add (tag)
 
         # Conflict miss
-        else :
+        else:
             self.conflict_miss_count += 1
             # TODO: Remove this kludgy workaround
-            # normal, expected case :
+            # normal, expected case:
             # tag not in cache, tag has been seen
             # that must mean the set is full
-            if (len (self.dict) == self.capacity) :
+            if (len (self.dict) == self.capacity):
                 self.evictAndWrite (tag, dirty_status)
-            # case at hand :
+            # case at hand:
             # write no allocate, wrote tag foo
             # that will mark tag foo as seen
             # read tag foo
             # the set has some space, still the execution will come here
             # and if the set is empty, evictAndWrite will fail.
             elif (self.write_no_allocate
-                  and dirty_status == self.clean) :
-                self.dict [tag] = self.clean 
-            else :
+                  and dirty_status == self.clean):
+                self.dict [tag] = self.clean
+            else:
                 sys.exit ("Logic fail! Should never come here.")
 
-    def write (self, tag) :
+    def write (self, tag):
         self.writeOrRead (tag, self.dirty)
 
-    def read (self, tag) :
+    def read (self, tag):
         self.writeOrRead (tag, self.clean)
-            
 
-class Cache (object) :
+
+class Cache (object):
     
     def __init__ (self,
                   n_sets,
@@ -127,7 +143,7 @@ class Cache (object) :
                   block_size,
                   sample_addr = "0xb7737f64",
                   set_class = Set,
-                  write_no_allocate = False) :
+                  write_no_allocate = False):
         self.n_sets = n_sets
         self.n_ways = n_ways
         self.block_size = block_size
@@ -148,14 +164,14 @@ class Cache (object) :
                                  write_no_allocate)
                       for foo in range (n_sets)]
 
-    def __splitAddr (self, addr) :
+    def __splitAddr (self, addr):
         """ Returns a tuple with tag, set_number.
         the memory is assumed to be byte addressable and further it
         is assumed that the address given by the addr is the byte
         address of the memory location. """
-        try :
+        try:
             addr = int (addr, 0)
-        except :
+        except:
             pass
         block_addr = addr / self.block_size
 
@@ -167,155 +183,197 @@ class Cache (object) :
         tag = block_addr / (2 ** self.set_number_bit_length)
         return (tag, set_number)
 
-    def __str__ (self) :
+    def __str__ (self):
         return "\n".join ([pad (hex (i) [2:],
                                 self.set_number_bit_length / 4)
-                           + " : "
+                           + ": "
                            + self.array[i].__str__()
                            for i in range (len (self.array))])
 
-    def write (self, addr) :
+    def write (self, addr):
         tag, set_number = self.__splitAddr (addr)
         self.array [set_number].write (tag)
 
-    def read (self, addr) :
+    def read (self, addr):
         tag, set_number = self.__splitAddr (addr)
         self.array [set_number].read (tag)
 
-    def printStats (self) :
+    def printStats (self):
         separator = "=================="
         print pad (self.__class__.__name__ + " specs", len (separator))
         print separator
         size = self.n_sets * self.n_ways * self.block_size
-        if size >= 1048576 :
+        if size >= 1048576:
             size = str (size / 1048576) + 'M'
-        elif size >= 1024 :
+        elif size >= 1024:
             size = str (size / 1024) + "K"
 
         block_size = self.block_size
-        if block_size >= 1048576 :
+        if block_size >= 1048576:
             block_size = str (block_size / 1048576) + 'M'
-        elif block_size >= 1024 :
+        elif block_size >= 1024:
             block_size = str (block_size / 1024) + "K"
-            
-        print "size       :", size
-        print "block size :", block_size
-        print "sets       :", self.n_sets
-        print "ways       :", self.n_ways
+
+        print "size      :", size
+        print "block size:", block_size
+        print "sets      :", self.n_sets
+        print "ways      :", self.n_ways
         print separator
         for count in ['access_count',
                       'cold_miss_count',
-                      'conflict_miss_count'] :
+                      'conflict_miss_count']:
             print (str (self.getCumulativeCount (count))
                    + '\t'
                    + ' '.join (count.split ('_')))
         miss = (self.getCumulativeCount ('cold_miss_count')
                 + self.getCumulativeCount ('conflict_miss_count'))
         total = self.getCumulativeCount ('access_count')
-        print ("Hit Rate : "
+        print ("Hit Rate: "
                + str (round (100 * (1 - float (miss) / total), 3))
                + "%")
 
-    def getCumulativeCount (self, attr_name) :
+    def getCumulativeCount (self, attr_name):
         return sum ([set_.__getattribute__(attr_name)
                      for set_ in self.array])
 
-class LRUSet (Set) :
-    def __init__ (self, *args, **kwargs) :
+    def getStats (self):
+        """Return list of vital cache statistics.
+
+        Statistics:
+        #accesses
+        #cold misses
+        #conflict misses
+        Hit Rate
+
+        TODO:
+        #capacity misses (???)
+        """
+        l1 = [self.getCumulativeCount (count)
+                for count in ['access_count',
+                              'cold_miss_count',
+                              'conflict_miss_count']]
+        l2 = [self.getHitRate ()]
+        return l1 + l2
+
+    def getParameters(self):
+        """Return list of parameters for this cache.
+
+        Return list of:
+        cache size (in bytes)
+        block size (in bytes)
+        #sets
+        #ways
+        """
+        size = self.n_sets * self.n_ways * self.block_size
+        return [size,
+                self.block_size,
+                self.n_sets,
+                self.n_ways]
+
+    def getHitRate(self):
+        """Return the % Hit Rate for cache.
+        """
+        miss = (self.getCumulativeCount ('cold_miss_count')
+                + self.getCumulativeCount ('conflict_miss_count'))
+        total = self.getCumulativeCount ('access_count')
+        return 100 * (1 - float (miss) / total)
+
+class LRUSet (Set):
+    def __init__ (self, *args, **kwargs):
         super(LRUSet, self).__init__ (*args, **kwargs)
         # [least recently used, ...., most recently used]
         self.recency_list = []
 
-    def pickBlockToEvict (self) :
+    def pickBlockToEvict (self):
         tag = self.recency_list [0]
         self.recency_list = self.recency_list [1:]
         return tag
 
-    def writeOrRead (self, tag, dirty_status) :
+    def writeOrRead (self, tag, dirty_status):
         super(LRUSet, self).writeOrRead (tag, dirty_status)
         # Remove the tag if it were there in the recency list
         # and put it at the end of the list.
-        try :
+        try:
             self.recency_list.remove (tag)
-        except :
+        except:
             # If it was a write miss, and it is write no allocate,
             # do nothing else.
             if (self.write_no_allocate
                 and tag not in self.dict
-                and dirty_status == self.dirty) : return
+                and dirty_status == self.dirty): return
         self.recency_list.append (tag)
 
-class LRUCache (Cache) :
-    def __init__ (self, *args, **kwargs) :
+class LRUCache (Cache):
+    def __init__ (self, *args, **kwargs):
         kwargs ['set_class'] = LRUSet
         super(LRUCache, self).__init__ (*args, **kwargs)
 
-class LFUSet (Set) :
-    def __init__ (self, *args, **kwargs) :
+class LFUSet (Set):
+    def __init__ (self, *args, **kwargs):
         super(LFUSet, self).__init__ (*args, **kwargs)
         # {tag:frequency}
         self.frequency_dict = {}
 
-    def pickBlockToEvict (self) :
+    def pickBlockToEvict (self):
         # Pick the tag with least frequency
         frequencies = self.frequency_dict.values ()
         frequencies.sort ()
         least_frequency = frequencies [0]
-        for tag in self.frequency_dict :
-            if self.frequency_dict [tag] == least_frequency :
+        for tag in self.frequency_dict:
+            if self.frequency_dict [tag] == least_frequency:
                 self.frequency_dict.pop (tag)
                 return tag
 
-    def writeOrRead (self, tag, dirty_status) :
+    def writeOrRead (self, tag, dirty_status):
         super(LFUSet, self).writeOrRead (tag, dirty_status)
 
         # If it was a write miss, and it is write no allocate,
         # do nothing else.
         if (self.write_no_allocate
             and tag not in self.dict
-            and dirty_status == self.dirty) : return
+            and dirty_status == self.dirty): return
 
         # Update the frequency value of that tag
-        try :
+        try:
             self.frequency_dict [tag] += 1
-        except :
+        except:
             self.frequency_dict [tag] = 1
 
-class LFUCache (Cache) :
-    def __init__ (self, *args, **kwargs) :
+class LFUCache (Cache):
+    def __init__ (self, *args, **kwargs):
         kwargs ['set_class'] = LFUSet
         super(LFUCache, self).__init__ (*args, **kwargs)
 
-class FIFOSet (Set) :
-    def __init__ (self, *args, **kwargs) :
+class FIFOSet (Set):
+    def __init__ (self, *args, **kwargs):
         super(FIFOSet, self).__init__ (*args, **kwargs)
         # [oldest tag added to set, ..., newest tag added to set]
         self.tag_queue = []
 
-    def pickBlockToEvict (self) :
+    def pickBlockToEvict (self):
         tag = self.tag_queue [0]
         self.tag_queue = self.tag_queue [1:]
         return tag
 
-    def writeOrRead (self, tag, dirty_status) :
+    def writeOrRead (self, tag, dirty_status):
         super(FIFOSet, self).writeOrRead (tag, dirty_status)
 
         # If it was a write miss, and it is write no allocate,
         # do nothing else.
         if (self.write_no_allocate
             and tag not in self.dict
-            and dirty_status == self.dirty) : return
+            and dirty_status == self.dirty): return
 
         # If it is a new tag, add it as the newest tag
-        if tag not in self.tag_queue :
+        if tag not in self.tag_queue:
             self.tag_queue.append (tag)
 
-class FIFOCache (Cache) :
-    def __init__ (self, *args, **kwargs) :
+class FIFOCache (Cache):
+    def __init__ (self, *args, **kwargs):
         kwargs ['set_class'] = FIFOSet
         super(FIFOCache, self).__init__ (*args, **kwargs)
 
-if __name__ == "__main__" :
+if __name__ == "__main__":
     foo = FIFOCache (16, 3, 1, '0xb00', write_no_allocate = False)
     foo.write (0xa2b)
     foo.write (0xa3c)
--- Simulator.py
+++ Simulator.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python
 
 import getopt
+import matplotlib.pyplot as plot
 import sys
 
 from Cache import *
@@ -80,36 +81,159 @@ def genMemtrace (filename):
     trace = [pair for pair in trace if len (pair) == 2]
     tracefile.close ()
     return trace
-    
 
+def ye_old_simulation_attempt ():
+    """Simulation attempt with lotsa combos of block size and #ways and whatnot.
+    
+    Combinatorial Explosion. Hilarity does not ensue.
+    """
+    cache_list = [Cache, FIFOCache, LRUCache, LFUCache]
+    true_false_list = [True, False]
+    matrix_size_list = [10, 20]
+    # matrix_size_list = range (10, 60, 10) # + [100]
+    cache_size_list = [1048576]
+    # cache_size_list = [524288, 1048576]
+    block_size_list = [16]
+    # block_size_list = [1, 4, 16]
+    ways_list = [4]
+    # ways_list = [1, 4, 16]
+    
+    # cache_size = cache_size_list[-1]
+    # block_size = block_size_list[-1]
+    # num_ways = ways_list[-1]
+    # for cache_type in cache_list:
+    #     cache = cache_type ((cache_size / block_size) / num_ways,
+    #                         num_ways,
+    #                         block_size,
+    #                         sample_addr,
+    #                         write_no_allocate = True)
+    #     simulate (cache, memtrace)
+    #     print cache.getStats ()
+    #     print cache.getParameters ()
+    #     # print '\n\n'
+    
+    total = 0
+    # Actually this should iterate over memtraces of multiplication of
+    # matrices of different sizes and with different blocking factors
+    for matrix_size in matrix_size_list:
+        # TODO: Use matrix size to read the memtrace for
+        # matrix mult of that size
+        for cache_type in cache_list:
+            for write_no_allocate in true_false_list:
+                for cache_size in cache_size_list:
+                    for block_size in block_size_list:
+                        for num_ways in ways_list:
+                            # cache = cache_type (num_sets,
+                            #                     num_ways,
+                            #                     block_size,
+                            #                     sample_addr,
+                            #                     write_no_allocate = True)
+                            
+                            total += 1
+                            print '\n'
+                            cache = cache_type ((cache_size / block_size) / num_ways,
+                                                num_ways,
+                                                block_size,
+                                                sample_addr,
+                                                write_no_allocate = write_no_allocate)
+                            simulate (cache, memtrace)
+                            # print cache.getStats ()
+                            # print cache.getParameters (), '\n'
+                            cache.printStats ()
+
+def write_result_to_file (filename, value_list):
+    """Write value_list to filename in a comma-separated fashion.
+    """
+    f = open (filename, 'w')
+    f.write ('\n'.join (','.join ((str (key), str (val))) 
+                        for key, val in value_list))
+    f.close ()
+
+def plot_cache_graphs (cache_list, blocking_factor_list):
+    """Plot a graph for each cache in cache_list.
+
+    Each graph will depict stats for all blocking factors in
+    blocking_factor_list.
+    """
+    for cache_type in cache_list:
+        plot_graph (cache_type, blocking_factor_list)
+        
+def plot_graph (cache_type, blocking_factor_list):
+    """
+    Plot graph 'Conflict Miss Rate vs Matrix Size' for given blocking
+    factor.
+
+    Read stats from 'results-<Cache name>-<Blocking factor>.txt' file.
+    """
+    for blocking_factor in blocking_factor_list:
+        filename = 'results-{0}-{1}.txt'.format (cache_type.__name__, blocking_factor)
+        f = open (filename, 'r')
+        size_miss_rate_list = [(int (line.split (',')[0]),
+                                float (line.split (',')[1]))
+                                for line in f]
+        f.close ()
+        x_vals, y_vals = zip (*size_miss_rate_list)
+        # print x_vals
+        # print y_vals
+        plot.plot (x_vals, y_vals, label = 'BF - {0}'.format (blocking_factor))
+    plot.xlabel ('Matrix Size')
+    plot.ylabel ('Conflict Miss Rate')
+    plot.legend (loc = 'upper right')
+    plot.title (r'Graph for Block Matrix Multiplication using {0}'.format (
+        cache_type.__name__))
+    plot.savefig ('results-{0}.png'.format (cache_type.__name__))
+    plot.close ()
+    
 if __name__ == "__main__":
-    readOptions (sys.argv [1:])
+    # readOptions (sys.argv [1:])
+    file_name = 'sample-mem-trace.txt'
     memtrace = genMemtrace (file_name)
     sample_addr = memtrace [0][1]
 
     cache_list = [Cache, FIFOCache, LRUCache, LFUCache]
     true_false_list = [True, False]
-    matrix_size_list = range (10, 60, 10) # + [100]
-    block_size_list = [16, 64, 256]
-    ways_list = [1, 4, 16]
+    # matrix_size_list = range (10, 60, 10) # + [100]
+    cache_size_list = [1048576]
+    # cache_size_list = [524288, 1048576]
+    block_size_list = [16]
+    # block_size_list = [1, 4, 16]
+    ways_list = [4]
+    # ways_list = [1, 4, 16]
 
-    for cache_type in cache_list:
-        for write_no_allocate in true_false_list:
-            for matrix_size in matrix_size_list:
-                for block_size in block_size_list:
-                    for num_ways in ways_list:
-                        # cache = cache_type (num_sets,
-                        #                     num_ways,
-                        #                     block_size,
-                        #                     sample_addr,
-                        #                     write_no_allocate = True)
-                        
-                        print 'Yo, boyz!'
-                        # cache = cache_type ((cache_size / block_size) / num_ways,
-                        #                     num_ways,
-                        #                     block_size,
-                        #                     # TODO: Check if this is necessary
-                        #                     sample_addr,
-                        #                     write_no_allocate = write_no_allocate)
-                        simulate (cache, memtrace)
-                        # print '\n\n'
+    blocking_factor_list = [1, 2, 4, 8, 16]
+    matrix_size_list = range (16, 100, 16)
+
+    if len (sys.argv) != 2:
+        print 'Enter exactly 1 argument out of:\nplot\nsimulate\nsimulate_all\n'
+        exit (1)
+
+    if sys.argv[1] == 'simulate':
+        output_dict = {}
+        for cache_type in cache_list:
+            output_dict[cache_type] = {}
+            cache_size = cache_size_list[-1]
+            block_size = block_size_list[-1]
+            num_ways = ways_list[-1]
+            cache = cache_type ((cache_size / block_size) / num_ways,
+                                num_ways,
+                                block_size,
+                                sample_addr,
+                                write_no_allocate = True)
+            for blocking_factor in blocking_factor_list:
+                output_dict[cache_type][blocking_factor] = []
+                for matrix_size in matrix_size_list:
+                    # TODO: Have actual memtraces in these files
+                    memtrace = genMemtrace ('memtrace-{0}-{1}.txt'.format (
+                        matrix_size, blocking_factor))
+                    simulate (cache, memtrace)
+                    output_dict[cache_type][blocking_factor].append (
+                        (matrix_size, cache.getHitRate ()))
+                # print output_dict[cache_type][blocking_factor]
+                write_result_to_file (
+                    'results-{0}-{1}.txt'.format (cache_type.__name__, blocking_factor),
+                    output_dict[cache_type][blocking_factor])
+    elif sys.argv[1] == 'plot':
+        plot_cache_graphs (cache_list, blocking_factor_list)
+    elif sys.argv[1] == 'simulate_all':
+        ye_old_simulation_attempt ()
+
